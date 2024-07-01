37ca82c57a866423bad948e3e6f6becd04927681
Angel Quach <angel.quach@uwaterloo.ca>
2024-07-01 18:30:37 -0400

    Added semaphores and related functions

--- lab3/common.h
+++ lab3/common.h
@@ -2,23 +2,20 @@
 #define COMMON_H
 
 #define _POSIX_C_SOURCE 200809L
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <sys/wait.h>
 #include <semaphore.h>
-#include <pthread.h>
 #include "recv_buf.h"
 
 #define BUF_SIZE 1048576    /* 1024*1024 = 1M */
 #define MAX_STRIPS 50       /* Total num of image strips */
-#define ECE252_HEADER "X-Ece252-Fragment: "
 
 /* To indicate if the strip has been downloaded */
-int is_done[50] = {0};
+int last_seq = 0;
+/* The semaphore to be shared among producers/consumers */
+sem_t prod_sem;
+sem_t cons_sem;
 
 typedef struct{
     char *uncomp_image;
@@ -30,11 +27,15 @@ typedef struct{
 
     int in;                 /* index for producer */
     int out;                /* index for consumer */
+    sem_t access_sem;       /* semaphore for accessing the last sequence downloaded */
     sem_t empty;            /* semaphore for empty slots */
     sem_t full;             /* semaphore for full slots */
 }CircularQueue;
 
+void init_semaphores(CircularQueue *image_queue, int buffer_size);
 void init_image_queue(CircularQueue *image_queue, int buffer_size);
 void cleanup_image_queue(CircularQueue *image_queue);
+
+void producer_process(CircularQueue *queue, int machine_num, int img_num);
 
 #endif
