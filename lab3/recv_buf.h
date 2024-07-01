56475b59536663859fa2a5bb169ae90dcc7a67e7
Angel Quach <angel.quach@uwaterloo.ca>
2024-07-01 19:41:52 -0400

    Fixed a few bugs

--- lab3/recv_buf.h
+++ lab3/recv_buf.h
@@ -1,18 +1,27 @@
+#ifndef RECV_BUF_H
+#define RECV_BUF_H
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <string.h>
 #include <stdarg.h>
 #include <sys/types.h>
-#include <time.h>
-#include <sys/time.h>
 
 #define uncomp_strip 9606  /* size of individual uncompressed strip */
 #define uncomp_img 480300  /* total size of uncompressed strips */
 
-typedef struct recv_buf;
+/* Buffer for storing image segments */
+typedef struct{
+    char *buf;
+    size_t size;
+    size_t max_size;
+    int seq;
+} recv_buf;
+
 recv_buf* init_image_buf();
+
+#endif
