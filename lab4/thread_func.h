34f0fa6e682032dfc4d19b9ffe1e4d995c70cdfd
Angel Quach <angel.quach@uwaterloo.ca>
2024-07-24 17:15:46 -0400

    Lab 4 Remark

--- lab4/thread_func.h
+++ lab4/thread_func.h
@@ -4,9 +4,9 @@
 #include "main.h"
 #include "data_structure.h"
 
-int process_data(CircularQueue *q, URL_Array *PNG_url, CURL *curl_handle, RECV_BUF *p_recv_buf);
-int process_png(CURL *curl_handle, URL_Array *PNG_url, RECV_BUF *p_recv_buf);
-int process_html(CircularQueue *q, CURL *curl_handle, RECV_BUF *p_recv_buf);
-int find_http(CircularQueue *q, char *buf, int size, int follow_relative_links, const char *base_url);
+int process_data(CircularQueue *q, URL_Array *PNG_url, pthread_mutex_t mutex_PNG, pthread_mutex_t mutex_HTTP, HashTable *url_visited, CURL *curl_handle, RECV_BUF *p_recv_buf);
+int process_png(CURL *curl_handle, URL_Array *PNG_url, pthread_mutex_t mutex, RECV_BUF *p_recv_buf);
+int process_html(CircularQueue *q, HashTable *url_visited, pthread_mutex_t mutex, CURL *curl_handle, RECV_BUF *p_recv_buf);
+int find_http(CircularQueue *q, HashTable *url_visited, pthread_mutex_t mutex, char *buf, int size, int follow_relative_links, const char *base_url);
 
 #endif
