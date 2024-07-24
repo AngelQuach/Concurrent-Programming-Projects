8cc765261b75d4d4adcaf10ce7a00e40a7ee7ecf
Angel Quach <angel.quach@uwaterloo.ca>
2024-07-24 17:15:20 -0400

    Lab 4 Remark

--- lab4/thread_func.c
+++ lab4/thread_func.c
@@ -7,7 +7,7 @@
  * @param RECV_BUF p_recv_buf contains the received data. 
  * @return 0 on success; non-zero otherwise
  */
-int process_data(CircularQueue *q, URL_Array *PNG_url, CURL *curl_handle, RECV_BUF *p_recv_buf)
+int process_data(CircularQueue *q, URL_Array *PNG_url, pthread_mutex_t mutex_PNG, pthread_mutex_t mutex_HTTP, HashTable *url_visited, CURL *curl_handle, RECV_BUF *p_recv_buf)
 {
     CURLcode res;
     long response_code;
@@ -29,15 +29,15 @@ int process_data(CircularQueue *q, URL_Array *PNG_url, CURL *curl_handle, RECV_B
 
     /* Check if it is a HTML or a PNG */
     if ( strstr(ct, CT_HTML) ) {
-        return process_html(q, curl_handle, p_recv_buf);
+        return process_html(q, url_visited, mutex_HTTP, curl_handle, p_recv_buf);
     } else if ( strstr(ct, CT_PNG) ) {
-        return process_png(curl_handle, PNG_url, p_recv_buf);
+        return process_png(curl_handle, PNG_url, mutex_PNG, p_recv_buf);
     }
     return 0;
 }
 
 /* Modified from starter code; ensure proper handling of png url */
-int process_png(CURL *curl_handle, URL_Array *PNG_url, RECV_BUF *p_recv_buf)
+int process_png(CURL *curl_handle, URL_Array *PNG_url, pthread_mutex_t mutex, RECV_BUF *p_recv_buf)
 {
     char *eurl = NULL;          /* effective URL */
     curl_easy_getinfo(curl_handle, CURLINFO_EFFECTIVE_URL, &eurl);
@@ -46,10 +46,10 @@ int process_png(CURL *curl_handle, URL_Array *PNG_url, RECV_BUF *p_recv_buf)
         char buf[8];
         memcpy(buf, p_recv_buf->buf, 8);
         if(buf[1] == 0x50 && buf[2] == 0x4E && buf[3] == 0x47){        /* Check if file is a png */
-            /* Add to png list */
-            while(addPNGURL(PNG_url, eurl) != 0);                      /* Busy adding url to the list */
-            //printf("Successfully added %s, %d\n", PNG_url->urls[PNG_url->count -1], (PNG_url->count) - 1);
-            printf("Now we have: %d\n", countPNGURL(PNG_url));
+            pthread_mutex_lock(&mutex);
+                /* Add to png list */
+                while(countPNGURL(PNG_url) < PNG_url->PNG_needed && addPNGURL(PNG_url, eurl) != 0);                      /* Busy adding url to the list */
+            pthread_mutex_unlock(&mutex);
         }
         return 0;
     } else{
@@ -58,18 +58,18 @@ int process_png(CURL *curl_handle, URL_Array *PNG_url, RECV_BUF *p_recv_buf)
 }
 
 /* Modified from starter code; ensure proper handling of html url */
-int process_html(CircularQueue *q, CURL *curl_handle, RECV_BUF *p_recv_buf)
+int process_html(CircularQueue *q, HashTable *url_visited, pthread_mutex_t mutex, CURL *curl_handle, RECV_BUF *p_recv_buf)
 {
     int follow_relative_link = 1;
     char *url = NULL; 
 
     curl_easy_getinfo(curl_handle, CURLINFO_EFFECTIVE_URL, &url);
-    find_http(q, p_recv_buf->buf, p_recv_buf->size, follow_relative_link, url); 
+    find_http(q, url_visited, mutex, p_recv_buf->buf, p_recv_buf->size, follow_relative_link, url); 
     return 0;
 }
 
 /* Modified from starter code; ensure all urls from http is copied to array */
-int find_http(CircularQueue *q, char *buf, int size, int follow_relative_links, const char *base_url)
+int find_http(CircularQueue *q, HashTable *url_visited, pthread_mutex_t mutex, char *buf, int size, int follow_relative_links, const char *base_url)
 {
     int i;
     htmlDocPtr doc = NULL;
@@ -97,8 +97,16 @@ int find_http(CircularQueue *q, char *buf, int size, int follow_relative_links, 
                 xmlFree(old);
             }
             if (href != NULL && !strncmp((const char *)href, "http", 4)) {
-                /* Add url to the url_frontiers array */
-                while(enqueue(q, (char *)href) != 0);       /* Busy calling enqueue till url is added to array */
+                /* Check if url has been added */
+                pthread_mutex_lock(&mutex);
+                if(checkHashURL(url_visited, (char *)href) == 0){
+                    /* If not visited add to url visited array */
+                    addHashURL(url_visited, (char *)href);
+
+                    /* Add url to the url_frontiers array */
+                    while(enqueue(q, (char *)href) != 0);       /* Busy calling enqueue till url is added to array */
+                }  /* If already added before, do nothing */
+                pthread_mutex_unlock(&mutex);
             }
             xmlFree(href);
         }
